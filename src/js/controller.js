57e0e3a4ec8391fa27f46074e67ea093ddb69130
yujioyama <you@example.com>
2023-06-08 19:07:25 +0900

    check api

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,23 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const response = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await response.json();
+
+    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+    let { recipe } = data.data;
+
+    console.log(recipe);
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+showRecipe();
